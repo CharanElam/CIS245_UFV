d58e542d041a007df9dc234c353ee443a12a8d4c
CharanElam <charanelamparithikala@gmail.com>
2025-03-27 04:00:53 -0700

    Added all the tables from SRD v1

--- CIS245_database.sql
+++ CIS245_database.sql
@@ -18,3 +18,23 @@ staff_email VARCHAR(100),
 staff_password VARCHAR(100),
 staff_role ENUM("Surgery","Anesthesia","Pathology") NULL
 );
+
+CREATE TABLE IF NOT EXISTS queue(
+queue_id INT PRIMARY KEY,
+patient_id INT,
+queue_position INT,
+added_by INT,
+queue_date DATE,
+queue_time TIME,
+priority INT,
+FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+FOREIGN KEY (added_by) REFERENCES staff(staff_id)
+);
+
+CREATE TABLE IF NOT EXISTS visit_records(
+record_id INT PRIMARY KEY,
+patient_id INT,
+queue_id INT,
+FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+FOREIGN KEY (queue_id) REFERENCES queue(queue_id)
+); 
